238cd1b56f06eb798bea15680cb63a7de2cf81f8
kiki-ke yang <holyground12@protonmail.com>
2022-04-27 11:13:42 +1000

    First Try To Push

--- app/src/main/java/com/autonomousapps/common/LogUtil.java
+++ app/src/main/java/com/autonomousapps/common/LogUtil.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+//try the github changes and push it to GitHub kiky
 public class LogUtil {
 
     public static void v(@NonNull String tag, @NonNull String msg, @Nullable Object... args) {
